7a63a001e9345bde86484e492382605b485d67c7
swarup999 <31533715+swarup999@users.noreply.github.com>
2024-11-26 13:24:16 +0530

    Create security_misconfiguration.cpp

---
+++ src/security_misconfiguration.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void securityMisconfigurationExample() {
+    std::cout << "Warning: Security misconfiguration detected!" << std::endl;
+    // Simulated misconfiguration logic
+}
