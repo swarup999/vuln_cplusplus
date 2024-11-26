6ea4e2ba6eebb09a106f7687b57a67ea602a173c
swarup999 <31533715+swarup999@users.noreply.github.com>
2024-11-26 14:06:50 +0530

    Update sensitive_data_exposure.cpp

--- src/sensitive_data_exposure.cpp
+++ src/sensitive_data_exposure.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
+#include "sensitive_data_exposure.h"
 
 void sensitiveDataExposureExample() {
     std::string password = "secret"; // Hardcoded sensitive data
     std::cout << "Password: " << password << std::endl; // Exposing sensitive data
+    std::cout << "This should not be printed in production!" << std::endl;
 }
